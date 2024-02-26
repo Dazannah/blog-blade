dac2b848ac794d8a3095dec63e6c6c04a5bfdb81
Dazannah <beattius@gmail.com>
2024-02-26 16:52:16 +0100

    dark and light theme color corrected

--- resources/views/new-post.blade.php
+++ resources/views/new-post.blade.php
@@ -23,18 +23,18 @@
                     @csrf
 
                     <label for="title">Title</label><br>
-                    <input id="title" name="title" type="text" class="text-black sm:rounded-lg" value="{{ old('title') }}"><br>
+                    <input id="title" name="title" type="text" class="text-black sm:rounded-lg dark:bg-gray-200" value="{{ old('title') }}"><br>
                     @error('title')
                       <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
 
                     <label for="post-body">Post body</label><br>
-                    <textarea name="post-body" id="post-body" cols="30" rows="10" class="text-black resize-none rounded-md">{{ old('post-body') }}</textarea><br>           
+                    <textarea name="post-body" id="post-body" cols="30" rows="10" class="text-black resize-none rounded-md dark:bg-gray-200">{{ old('post-body') }}</textarea><br>           
                     @error('post-body')
                       <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
 
-                    <button type="submit" class="py-2 bg-transparent border border-gray-400 hover:border-transparent hover:bg-gray-900  text-xl text-gray-800 dark:text-gray-200 font-bold py-2 px-4 rounded">
+                    <button type="submit" class="py-2 bg-transparent border border-gray-400 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-900 text-xl text-gray-800 dark:text-gray-200 font-bold py-2 px-4 rounded">
                       Submit
                     </button>
 
