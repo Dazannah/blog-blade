552a2aedd7bb40e888a99c061fa2060f32f879b4
Dazannah <beattius@gmail.com>
2024-02-26 16:02:23 +0100

    updated the public test user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <div class="sm:justify-center sm:items-center min-h-screen bg-darker bg-center bg-gray-100 dark:bg-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @include('layouts.guestNavigation')
             <div class="font-semibold text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                Welcome to "My blog", feel free to register and try out the site with a new account, or try it with guest/guest.
+                Welcome to "My blog", feel free to register and try out the site with a new account, or try it with teszt/Teszt1234.
             </div>
 
             <div class="text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
