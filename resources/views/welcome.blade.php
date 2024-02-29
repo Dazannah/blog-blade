b2c2c471b262d4d8265b2b36124e1eef5a74636c
Dazannah <beattius@gmail.com>
2024-02-29 17:27:30 +0100

    welcome modifyed to use app layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+  <x-slot:pageTitle>{{$pageTitle}}</x-slot:pageTitle>
 
-        <title>Welcome | {{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="sm:justify-center sm:items-center min-h-screen bg-darker bg-center bg-gray-100 dark:bg-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @include('layouts.guestNavigation')
+  <div class="sm:justify-center sm:items-center min-h-screen bg-darker bg-center bg-gray-100 dark:bg-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <div class="font-semibold text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                Welcome to "My blog", feel free to register and try out the site with a new account, or try it with teszt/Teszt1234.
+                Welcome to "My blog", feel free to register and try out the site with a new account, or try it with teszt@davidfabian.hu/Teszt1234.
             </div>
 
             <div class="text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -26,5 +12,4 @@
 
             @include('components.posts')
         </div>
-    </body>
-</html>
+</x-app-layout>
