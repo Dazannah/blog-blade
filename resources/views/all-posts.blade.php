d12c1cb733c5f1a83941981f062a2e7a8f7374b4
Dazannah <beattius@gmail.com>
2024-02-29 18:42:23 +0100

    al posts frontend

--- resources/views/all-posts.blade.php
+++ resources/views/all-posts.blade.php
@@ -1,3 +1,12 @@
 <x-app-layout>
   <x-slot:pageTitle>{{$pageTitle}}</x-slot:pageTitle>
+
+  <x-slot name="header">
+     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('All posts') }}
+    </h2>
+  </x-slot>
+  @include('components.filter-posts')
+  @include('components.posts')
+  @include('components.pagination')
 </x-app-layout>
