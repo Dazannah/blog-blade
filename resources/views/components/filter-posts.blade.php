46d41464dc8104e97ed67c190e726cbbc7d72e27
Dazannah <beattius@gmail.com>
2024-02-29 18:42:48 +0100

    filter posts created

---
+++ resources/views/components/filter-posts.blade.php
@@ -0,0 +1,16 @@
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-3">
+  <div class="font-semibold text-xl leading-tight">
+    <form action="/all-posts" method="post">
+      <input class="rounded" type="text" name="author" id="author" placeholder="Author">
+      <input class="rounded" type="text" name="post-title" id="post-title-search" placeholder="Post title">
+      <input class="rounded" type="text" name="post-body" id="post-body-search" placeholder="Post body">
+      <input class="rounded cursor-pointer" type="date" name="date" id="date">
+      <button type="submit" class="py-1 px-3 border border-gray-400 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800 text-xl text-gray-800 dark:text-gray-200 rounded" >
+        <!--<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>-->
+        Search
+      </button>
+    </form>
+  </div>
+</div>
