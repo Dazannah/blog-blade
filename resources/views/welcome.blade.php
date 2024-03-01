75ec71a8532b69b1aba4390e3a4a427eda8f7fb9
Dazannah <beattius@gmail.com>
2024-03-01 17:52:31 +0100

    changed the spacing a bit, replaced the last wort to latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,12 @@
   <x-slot:pageTitle>{{$pageTitle}}</x-slot:pageTitle>
 
   <div class="sm:justify-center sm:items-center min-h-screen bg-darker bg-center bg-gray-100 dark:bg-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            <div class="font-semibold text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div class="font-semibold text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8">
                 Welcome to "My blog", feel free to register and try out the site with a new account, or try it with teszt@davidfabian.hu/Teszt1234.
             </div>
 
-            <div class="text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                Last 10 new blog posts:
+            <div class="text-gray-600 dark:text-gray-400 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-8">
+                Latest 10 new blog posts:
             </div>
 
             @include('components.posts')
